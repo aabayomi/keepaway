0e0202f5a82bc48289a188395dcdb138b77554a0
Aijun Bai <aijunbai@gmail.com>
2017-08-27 10:24:16 -0700

    updated batch-train.sh

--- batch-train.sh
+++ batch-train.sh
@@ -25,10 +25,10 @@ make -j `nproc` release
 exec 1>console.log 2>&1                                                              
 
 for i in `seq 5`; do
-    gamma=$(python -c "import random; print(random.uniform(0.9, 1.0))")
-    lambda=$(python -c "import random; print(random.uniform(0.0, 0.5))")
-    alpha=$(python -c "import random; print(random.uniform(0.005, 0.2))")
-    initialweight=$(python -c "import random; print(random.uniform(0.0, 1.0))")
+    gamma=$(python -c "import random; print('{:.3f}'.format(random.uniform(0.9, 1.0)))")
+    lambda=$(python -c "import random; print('{:.3f}'.format(random.uniform(0.0, 0.5)))")
+    alpha=$(python -c "import random; print('{:.3f}'.format(random.uniform(0.005, 0.2)))")
+    initialweight=$(python -c "import random; print('{:.3f}'.format(random.uniform(0.0, 1.0)))")
 
     ./train.sh -b none -sf -g $gamma -L $lambda -A $alpha -I $initialweight $* & #hamq
     ./train.sh -b none -sf -g $gamma -L $lambda -A $alpha -I $initialweight $* -T & #hamq-int
