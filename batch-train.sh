01421846529b1489aca2ae41f85e1fec142386bc
Aijun Bai <aijunbai@gmail.com>
2017-09-02 14:52:06 -0700

    updated batch-train.sh

--- batch-train.sh
+++ batch-train.sh
@@ -20,7 +20,7 @@
 set -o nounset                              # Treat unset variables as an error
 
 NPROC=`nproc`
-N=`expr $NPROC / 2`
+N=`expr $NPROC / 2  - 1`
 
 make clean
 make -j `nproc` release
@@ -29,9 +29,10 @@ exec 1>console.log 2>&1
 
 ./train.sh -b none -sf -g 1.0 -L 0.5 -A 0.125 -I 0.5 $* & #hamq
 
+gamma=1.0
+
 for i in `seq $N`; do
-    gamma=$(python -c "import random; print('{:.3f}'.format(random.uniform(0.9, 1.0)))")
-    lambda=$(python -c "import random; print('{:.3f}'.format(random.uniform(0.0, 0.5)))")
+    lambda=$(python -c "import random; print('{:.3f}'.format(random.uniform(0.0, 1.0)))")
     alpha=$(python -c "import random; print('{:.3f}'.format(random.uniform(0.001, 0.25)))")
     initialweight=$(python -c "import random; print('{:.3f}'.format(random.uniform(0.0, 1.0)))")
 
